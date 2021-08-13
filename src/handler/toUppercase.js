9691aef915a7712eca7339a7059cb85f3731819b
dekk <dekkservidores@gmail.com>
2021-08-12 22:27:15 -0500

    feat() add headers

--- src/handler/toUppercase.js
+++ src/handler/toUppercase.js
@@ -4,6 +4,11 @@ const toUppercasefunc = async (event) => {
     let myWord = event.word.toUpperCase();
     const response = {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body: JSON.stringify({
             word: myWord
         }),
