86aef97218ad28bcfcfd5bad27116fd600e6fb44
dekk <dekkservidores@gmail.com>
2021-08-12 21:56:36 -0500

    fix() fix serverless.html

--- src/handler/thetime.js
+++ src/handler/thetime.js
@@ -1,12 +1,11 @@
 'use strict';
 
 module.exports.handler = async (event) => {
-    let time = new Date();
     return {
         statusCode: 200,
         body: JSON.stringify({
             message: 'Clever cabro',
-            body: time
+            body: new Date()
         })
     }
 }
