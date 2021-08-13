0b290128c741acd8360a87526cf3f8a01f44e39f
dekk <dekkservidores@gmail.com>
2021-08-12 21:52:08 -0500

    Fix() changes

--- src/handler/thetime.js
+++ src/handler/thetime.js
@@ -2,13 +2,11 @@
 
 module.exports.handler = async (event) => {
     let time = new Date();
-    const response = {
+    return {
         statusCode: 200,
         body: JSON.stringify({
             message: 'Clever cabro',
             body: time
         })
     }
-
-    return response;
 }
