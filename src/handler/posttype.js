8a4ab9ad57970875f7c2542b343ada4c8fe2b9fc
dekk <dekkservidores@gmail.com>
2021-08-14 14:49:46 -0500

    feat() add headers i posttype function

--- src/handler/posttype.js
+++ src/handler/posttype.js
@@ -5,6 +5,11 @@ const posttype = async (event) => {
 
     const response = {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body: JSON.stringify({
             data: body
         })
