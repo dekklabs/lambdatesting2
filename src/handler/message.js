988d2e4a86655ef1ba7769f61cbe15e9c7e79cff
Jonathan <dekkservidores@gmail.com>
2021-08-25 21:12:15 -0500

    feat() testing variable

--- src/handler/message.js
+++ src/handler/message.js
@@ -1,11 +1,13 @@
 'use strict'
 
 module.exports.handler = async (event) => {
+    let persona = "Jonathan";
     return {
         statusCode: 200,
         body: JSON.stringify({
             name: "Dekk",
-            body: "Working :D"
+            body: "Working :D",
+            lastname: persona,
         })
     }
 }
